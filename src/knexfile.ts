94c1aeb47f89b8f758270168a5d47fe34d68d6d6
Igor Santos <quimerafiresao@gmail.com>
2025-02-26 16:06:35 -0300

    alteração arquivo knex

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -3,7 +3,11 @@ import type { Knex } from "knex";
 const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./database.sqlite",
+    filename: "./src/database.sqlite", 
+  },
+  migrations: {
+    directory: "./src/migrations", 
+    extension: "ts", 
   },
   useNullAsDefault: true,
 };
