179cc1c0892a69ddc7053b20c1fb2b71027bcc68
Igor Santos <quimerafiresao@gmail.com>
2025-02-28 12:08:33 -0300

    manutenção

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,1 @@
-import winston from "winston";
 
-
-const logger = winston.createLogger({
-    level: "info", 
-    format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.printf(
-            (info) => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-    ),
-    transports: [
-        new winston.transports.Console(), 
-        new winston.transports.File({ filename: "logs/app.log" }) 
-    ]
-});
-
-export { logger };
